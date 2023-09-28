0052c0feee115a80cb004733d03d967f2f4ad8e9
Neto test <aryamorozkova555@gmail.com>
2023-09-28 20:20:25 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/test/BankLoginTest.java
@@ -0,0 +1,58 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.data.SQLHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.web.data.SQLHelper.cleanDatabase;
+
+public class BankLoginTest {
+
+    LoginPage loginPage;
+
+    @AfterEach
+    void tearDown() {
+        cleanDatabase();
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        cleanDatabase();
+    }
+
+    @BeforeEach
+    void setUp() {
+        loginPage = open("http://localhost:9999", LoginPage.class);
+    }
+
+    @Test
+    @DisplayName("Should success login to dashboard with exist login and password from sut test data")
+    void ShouldSuccessLogin() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+    @Test
+    @DisplayName("Should get error notification if user is not exist in base")
+    void ShouldGetErrorNotificationIfUserIsNotExistInBase() {
+        var authInfo = DataHelper.generateRandomUser();
+        loginPage.validLogin(authInfo);
+        loginPage.verifyErrorNotification("Ошибка! \nНеверно указан логин или пароль");
+    }
+
+    @Test
+    @DisplayName("Should get error notification if user exist in base and random verification code")
+    void ShouldGetErrorNotificationIfUserExistInBaseAndRandomVerificationCode() {
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = DataHelper.generateRandomVerificationCode();
+        verificationPage.verify(verificationCode.getCode());
+        verificationPage.verifyErrorNotification("Ошибка! \nНеверно указан код! Попробуйте ещё раз. ");
+    }
+}
